5fcec4364869ef0b26bc76dfb34f53d29580cd7e
MaksymKalennyk <107300165+MaksymKalennyk@users.noreply.github.com>
2023-05-22 13:00:44 +0300

    header

---
+++ src/app/currency/currency.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {CurrencyService} from "./CurrencyService";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-currency',
+  templateUrl: './currency.component.html',
+  styleUrls: ['./currency.component.css']
+})
+export class CurrencyComponent {
+  usdRate: number | undefined;
+  eurRate: number | undefined;
+  apiData: any;
+
+  constructor(private currencyService: CurrencyService, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    const apiUrl = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json'; // Замените на фактическую ссылку на ваше API
+
+    this.http.get(apiUrl).subscribe(data => {
+      this.apiData = data;
+      this.extractData();
+    });
+  }
+
+  extractData() {
+    const requiredDataUsd = this.apiData.find((item: { cc: string; }) => item.cc === 'USD');
+    const requiredDataEur = this.apiData.find((item: { cc: string; }) => item.cc === 'EUR');
+    if (requiredDataUsd) {
+      console.log(requiredDataUsd);
+       this.usdRate = requiredDataUsd.rate;
+      console.log(this.usdRate)
+    }
+    if (requiredDataEur) {
+      console.log(requiredDataEur);
+      this.eurRate = requiredDataEur.rate;
+      console.log(this.eurRate)
+    }
+  }
+}
